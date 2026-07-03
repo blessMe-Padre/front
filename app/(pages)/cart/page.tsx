39b1612a9e6bf0884e755d6fe1d6f3a24df795a2
Сергей Субботин <zarodiny@yandex.ru>
2026-07-03 11:12:15 +1000

    feat: добавлет стили корзины

--- app/(pages)/cart/page.tsx
+++ app/(pages)/cart/page.tsx
@@ -6,12 +6,14 @@ import useCartStore from "@/app/store/cartStore";
 
 import Image from "next/image";
 import styles from './style.module.scss';
+import Link from "next/link";
 
 export default function Cart() {
     const cartItems = useCartStore((state) => state.cartItems);
     const increaseQuantity = useCartStore((state) => state.increaseQuantity);
     const decreaseQuantity = useCartStore((state) => state.decreaseQuantity);
     const removeFromCart = useCartStore((state) => state.removeFromCart);
+    const clearCart = useCartStore((state) => state.clearCart);
 
     const imageServer = process.env.NEXT_PUBLIC_IMAGE_SERVER ?? "";
 
@@ -30,59 +32,89 @@ export default function Cart() {
         <Breadcrumbs secondLink='/catalog' secondLabel='Каталог' thirdLabel="корзина" />
 
         <div className="container">
-            <h1>Корзина</h1>
+            <h1 className={styles.cart_title}>Корзина</h1>
 
             {cartItems.length > 0 ? (
-                <ul className={styles.cart_list}>
-                    {cartItems.map((item) => (
-                        <li key={item.id} className={styles.cart_item}>
-                            <div className={styles.cart_item_info}>
-                                <div className={styles.cart_item_image}>
-                                    <Image src={imageServer + (item.images?.[0]?.url ?? '')} alt={item.title ?? ''} width={100} height={100} />
-                                </div>
+            <div className={styles.cart_wrapper}>
+                <div>
+                    {cartItems.length > 0 ? (
+                        <ul className={styles.cart_list}>
+                            {cartItems.map((item) => (
+                                <li key={item.id} className={styles.cart_item}>
+                                    <div className={styles.cart_item_info}>
+                                        <div className={styles.cart_item_image}>
+                                            <Image src={imageServer + (item.images?.[0]?.url ?? '')} alt={item.title ?? ''} width={100} height={100} />
+                                        </div>
 
-                                <div className={styles.cart_item_content}>
-                                    <h2 className={styles.cart_item_title}>{item.title}</h2>
-                                </div>
-                            </div>
+                                        <div className={styles.cart_item_content}>
+                                            <h2 className={styles.cart_item_title}>{item.title}</h2>
+                                        </div>
+                                    </div>
 
-                            <div className={styles.cart_item_actions}>
-                                <div className={styles.cart_item_counter}>
-                                    <div className={styles.counter_wrapper}>
-                                        <button type="button" onClick={() => decreaseQuantity(item.id)}>
-                                            -
-                                        </button>
-                                        <span>{item.quantity}</span>
-                                        <button type="button" onClick={() => increaseQuantity(item.id)}>
-                                            +
+                                    <div className={styles.cart_item_actions}>
+                                        <div className={styles.cart_item_counter}>
+                                            <div className={styles.counter_wrapper}>
+                                                <button type="button" onClick={() => decreaseQuantity(item.id)} className={styles.counter_button} title="уменьшить количество">
+                                                    <svg width="12" height="2" viewBox="0 0 12 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M11.0833 1.58333H0V0H11.0833V1.58333Z" fill="black" />
+                                                    </svg>
+                                                </button>
+                                                <span>{item.quantity}</span>
+                                                <button type="button" onClick={() => increaseQuantity(item.id)} className={styles.counter_button} title="увеличить количество">
+                                                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M11.0833 6.33333H6.33333V11.0833H4.75V6.33333H0V4.75H4.75V0H6.33333V4.75H11.0833V6.33333Z" fill="black" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                            <span>{(item.price ?? 0).toLocaleString('ru-RU')} ₽</span>
+                                        </div>
+
+                                        <div className={styles.price_wrapper}>
+                                            <span className={styles.cart_item_price}>
+                                            {((item.price ?? 0) * item.quantity).toLocaleString('ru-RU')} ₽
+                                            </span>
+                                        </div>
+
+                                        <button
+                                            type="button" onClick={() => removeFromCart(item.id)}
+                                            title="удалить товар из корзины"
+                                            className={styles.cart_item_remove}
+                                        >
+                                        <Image src="/icons/trash.svg" alt="trash" width={24} height={24} />
                                         </button>
                                     </div>
-                                    <span>{(item.price ?? 0) }</span>
-                                </div>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>Корзина пуста</p>
+                    )}
+                </div>
 
-                                <div className={styles.price_wrapper}>
-                                    <span>{(item.price ?? 0) * item.quantity}</span>
-                                </div>
-
-                                <button
-                                    type="button" onClick={() => removeFromCart(item.id)}
-                                    title="удалить товар из корзины"
-                                    className={styles.cart_item_remove}
-                                >
-                                   <Image src="/icons/trash.svg" alt="trash" width={24} height={24} />
-                                </button>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Корзина пуста</p>
-            )}
-
-            <div>
-                <p>Товаров в корзине: {counter}</p>
-                <p>Итого: {price}</p>
+                <div className={styles.cart_total_wrapper}>
+                    <div className={styles.cart_total}>
+                        <p className={styles.cart_total_price}>Итого: <span>{price.toLocaleString('ru-RU') ?? 0} ₽</span></p>
+                        <p className={styles.cart_total_counter}>Товаров в корзине: {counter}</p>
+                        <Link href="/checkout" className={styles.cart_checkout_button}>Оформить заказ</Link>
+                    </div>
+                    <button 
+                        type="button" 
+                        className={styles.cart_remove_all}
+                        title="Удалить все товары из корзины"
+                        onClick={() => clearCart()}
+                        >Очистить корзину
+                    </button>
+                </div>
             </div>
+            ) : (
+            <div className={styles.cart_empty}>
+                <div className={styles.cart_empty_wrapper}>
+                    <Image src="/icons/cart-empty.svg" alt="корзина пуста" width={220} height={206} />
+                    <p>В Вашей корзине пока нет товаров</p>
+                    <p><Link href="/catalog" className={styles.cart_empty_button}>Перейти в каталог, </Link>чтобы начать покупки</p>
+                </div>
+            </div>
+            )}
         </div>
         </>
     );
